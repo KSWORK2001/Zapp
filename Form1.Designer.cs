11d94dc8f8d301836c0ed966c064989f3ce81c0f
Karan Shrivastava <work.karan2001@gmail.com>
2023-09-30 23:35:14 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Zapp
+{
+    partial class Form1 : Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowserPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webBrowserPanel
+            // 
+            resources.ApplyResources(this.webBrowserPanel, "webBrowserPanel");
+            this.webBrowserPanel.Name = "webBrowserPanel";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Name = "panel1";
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Zapp.Properties.Resources.Screenshot_2023_09_30_190401;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webBrowserPanel);
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel webBrowserPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Drawing;
+using System.IO;
+
+namespace Zapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Specify the cache directory in the user's documents folder
+            string cachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "CefSharpCache");
+
+            // Ensure the cache directory exists, or create it if it doesn't
+            if (!Directory.Exists(cachePath))
+            {
+                Directory.CreateDirectory(cachePath);
+            }
+
+            // Initialize CefSharp with custom settings
+            CefSettings cefSettings = new CefSettings
+            {
+                CachePath = cachePath, // Set the cache directory
+            };
+
+            Cef.Initialize(cefSettings);
+
+            this.Size = new Size(1280, 720);
+
+            // Create and configure the ChromiumWebBrowser control for Discord.com
+            ChromiumWebBrowser chromiumWebBrowser = new ChromiumWebBrowser("https://www.discord.com/login");
+            chromiumWebBrowser.Dock = DockStyle.Fill;
+
+            // Add the ChromiumWebBrowser control to the webBrowserPanel
+            webBrowserPanel.Controls.Add(chromiumWebBrowser);
+
+            // Calculate the width of the panel as 1/3 of the form's width
+            int panelWidth = 1281 / 3;
+
+            // Set the width of the panel
+            webBrowserPanel.Size = new Size(panelWidth, this.Height);
+
+            ChromiumWebBrowser chromiumWebBrowser2 = new ChromiumWebBrowser("https://www.dopebox.to");
+            chromiumWebBrowser2.Dock = DockStyle.Fill;
+
+            panel2.Controls.Add(chromiumWebBrowser2);
+
+            // Set the initial zoom level using JavaScript
+            chromiumWebBrowser.FrameLoadEnd += (sender, args) =>
+            {
+                string jsCode = "document.body.style.zoom = '100%';"; // Adjust the zoom level as needed
+                chromiumWebBrowser.ExecuteScriptAsync(jsCode);
+            };
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // Shutdown CefSharp when closing the form
+            Cef.Shutdown();
+            base.OnFormClosing(e);
+        }
+
+        private void Form1_SizeChanged(object sender, System.EventArgs e)
+        {
+            int panelWidth = this.Width / 3;
+
+            // Set the width and position of the panel
+            webBrowserPanel.Width = panelWidth;
+            webBrowserPanel.Left = this.Width - panelWidth;
+            int newPanel1Width = this.Width - webBrowserPanel.Width;
+            panel1.Width = newPanel1Width;
+
+            int newPanel2Width = webBrowserPanel.Left - panel2.Left;
+            int newPanel2Height = panel1.Top - panel2.Top;
+            panel2.Size = new Size(newPanel2Width, newPanel2Height);
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
